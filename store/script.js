1a7a38bce09834f7a12a59e66c9a16e626aa5a0b
Christina Renieris <christinarenieris@gmail.com>
2020-02-11 13:12:52 +0100

    fixing style

--- store/script.js
+++ store/script.js
@@ -1,11 +1,11 @@
 var food = JSON.parse(groceries);
 for (var i = 0; i < food.length; i++) {
     $(".itemsContainer").append(
-        `<div id="item${food[i].ID}">
-        <p> ${food[i].img} </p>
+        `<div class="box" id="item${food[i].ID}">
+        <div class="pic"> ${food[i].img} </div>
         <p>Name : ${food[i].name} </p>
-        <p>Price : ${food[i].price} </p>
-        <button type="submit">Add to Cart </button>
+        <p>Price : € ${food[i].price} </p>
+        <button class="btn" type="submit">Add to Cart </button>
        </div>`
     )
 };
