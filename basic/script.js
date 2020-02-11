6584c750846ff8dcc393c233286900d74dfa9c2a
sarahvic <sarahley93@gmail.com>
2020-02-11 10:51:40 +0100

    basic

---
+++ basic/script.js
@@ -0,0 +1,19 @@
+// 1
+// $("p").css("display", "none");
+
+// $(document).ready(function(){
+// 	$("div.pic").click(function(){
+// 		var content = $(this).text();
+// 		$(".statement").text("You clicked on the " + content);
+// 	})
+
+// })
+
+
+//2
+$(document).ready(function(){
+	$(".santa").click(function(){
+		$(this).hide()
+	})
+
+})
